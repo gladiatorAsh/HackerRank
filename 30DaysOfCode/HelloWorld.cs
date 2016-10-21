78635530716dc4679194200838756a344c906aeb
Ashutosh Singh <ashutosh.singh@sjsu.edu>
2016-10-20 17:47:27 -0700

    Create HelloWorld.cs

---
+++ 30DaysOfCode/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class Solution{
+  public static void main(String[] args){
+    String inputString=Console.ReadLine();
+    
+    Console.WriteLine("Hello, World !");
+    Console.WriteLine(inputString);
+  }
+}
