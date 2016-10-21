3c72381bdd0b98eafcdce2669b7255a32e4cf532
Ashutosh Singh <ashutosh.singh@sjsu.edu>
2016-10-20 17:42:18 -0700

    Create 30DaysOfCode\HelloWorld.java

---
+++ 30DaysOfCode\HelloWorld.java
@@ -0,0 +1,12 @@
+public class Solution{
+  public static void main(String[] args){
+    Scanner scan=new Scanner(System.in);
+    
+    String inputLine=scan.nextLine();
+    
+    scan.close();
+    
+    Sytem.out.println("Hello world!");
+    System.out.println(inputLine);
+  }
+}
