431790b9cd666f75e3d1935d3946d720db105929
Ashutosh Singh <ashutosh.singh@sjsu.edu>
2018-09-23 19:49:02 -0700

    Update QueueWithTwoStacks.java

--- QueueWithTwoStacks.java
+++ QueueWithTwoStacks.java
@@ -9,15 +9,15 @@
   Subsequent dequeues can be easily done on the 2nd stack
   Once 2nd stack is empty, again dump the entire 1st stack onto 2nd and process repeats
   
-  Dequeue: Double ended queue. Can be used for implementing both Queues and Stacks.
+  Deque: Double ended queue. Can be used for implementing both Queues and Stacks.
   
 */
 
 import java.util.*;
 
 public static  class QueueToStacks{
-    private Dequeue<Integer> inStack=new Array<Dequeue>();
-    private Dequeue<Integer> outStack=new Array<Dequeue>();
+    private Deque<Integer> inStack=new Array<Deque>();
+    private Deque<Integer> outStack=new Array<Deque>();
 
     public void enqueue(int item){
         inStack.push(item);
